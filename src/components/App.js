6dc1c80e20fc10dd39c233ec34a1baa320182370
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-16 12:11:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ function App () {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+    left: 0,
+    top:0,
   });
   function buttonClickHandler(){
     setRenderBall(true);
   }
   const reset = () => {setRenderBall(false)
-  setBallPosition({left:"0px",top:"0px"});
+  setBallPosition({left:0,top:0});
   setX(0);
   setY(0);
   };
@@ -23,32 +23,44 @@ function App () {
     }
       
       else if(!renderBall) {
-        return <button onClick={buttonClickHandler} >Click For One Ball</button>
+        return <button className="start"  onClick={buttonClickHandler} >Click For One Ball</button>
       }
   };
-    window.addEventListener("keydown",update);
+    document.addEventListener("keydown",update);
 
 function update(event){
   if(event.key==="ArrowRight"){
-      setBallPosition(prv=>({left:prv.left+5},{top:prv.top}))
-     setX(x+5)
+      setBallPosition(prv=>({
+          
+          left:prv.left+5,
+          top:prv.top}))
+    //  setX(x+5)
      console.log("button right")
   }
   else if(event.key==="ArrowLeft"){
-   setBallPosition(prv=>({left:prv.left-5},{top:prv.top}))
-  setX(x-5)
+    setBallPosition(prv=>({
+          
+        left:prv.left-5,
+        top:prv.top}))
+//   setX(x-5)
 
   console.log("button left")
 
 }
 else if(event.key==="ArrowUp"){
- setBallPosition(prv=>({left:prv.left},{top:ballPosition.top-5}))
-setY(y+5)
+    setBallPosition(prv=>({
+          
+        left:prv.left,
+        top:prv.top-5}))
+// setY(y+5)
 console.log("button up")
 }
 else if(event.key==="ArrowDown"){
-setBallPosition(prv=>({left:prv.left},{top:prv.top+5}))
-setY(y-5)
+    setBallPosition(prv=>({
+          
+        left:prv.left,
+        top:prv.top+5}))
+// setY(y-5)
 console.log("button down")
 }
 }
