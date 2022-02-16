a0d4e525a6bceda6746c3fff039d3b7398c5da3d
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-16 11:28:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState,useEffect } from "react";
-import "../styles/App.css";
+import "./styles/App.css";
 
-const App = () => {
+function App () {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
@@ -13,35 +13,43 @@ const App = () => {
     setRenderBall(true);
   }
   const reset = () => {setRenderBall(false)
-  setBallPosition({left:"0px",top:"0px"})
+  setBallPosition({left:"0px",top:"0px"});
+  setX(0);
+  setY(0);
   };
-  const renderChoice = () => {
-    if (renderBall) 
-      return <div className="ball" style={{position:"absolute",left:ballPosition.left,top:ballPosition.top}}></div> 
+  function renderChoice(){
+    if (renderBall==true) {
+        return <div className="ball" style={{position:"absolute",left:ballPosition.left,top:ballPosition.top}}></div> 
+    }
+      
       else if(!renderBall) {
         return <button onClick={buttonClickHandler} >Click For One Ball</button>
       }
   };
-  useEffect(()=>{
-    document.addEventListener("keydown",update)
-},[])
-function update(e){
-  if(e.key==="ArrowRight"){
-      setBallPosition(prv=>({left:prv.left+5,
-     top:prv.top}))
+    window.addEventListener("keydown",update);
+
+function update(event){
+  if(event.key==="ArrowRight"){
+      setBallPosition(prv=>({left:prv.left+5},{top:prv.top}))
+     setX(x+5)
+     console.log("button right")
   }
-  if(e.key==="ArrowLeft"){
-   setBallPosition(prv=>({left:prv.left-5,
-  top:prv.top}))
+  else if(event.key==="ArrowLeft"){
+   setBallPosition(prv=>({left:prv.left-5},{top:prv.top}))
+  setX(x-5)
+
+  console.log("button left")
 
 }
-if(e.key==="ArrowUp"){
- setBallPosition(prv=>({left:prv.left,
-top:prv.top-5}))
+else if(event.key==="ArrowUp"){
+ setBallPosition(prv=>({left:prv.left},{top:ballPosition.top-5}))
+setY(y+5)
+console.log("button up")
 }
-if(e.key==="ArrowDown"){
-setBallPosition(prv=>({left:prv.left,
-top:prv.top+5}))
+else if(event.key==="ArrowDown"){
+setBallPosition(prv=>({left:prv.left},{top:prv.top+5}))
+setY(y-5)
+console.log("button down")
 }
 }
 
